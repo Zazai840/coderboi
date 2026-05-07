85d7b37983358d1447509e05a8b99ec2e67a2104
Abdulaziz Zazai <zazai@Abdulazizs-MacBook-Air.local>
2026-05-07 14:43:02 +0100

    feat: Add multiple messaging and verbose prompt capability

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from coderboi!")
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+import argparse
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("add API key to project .env file from Google AI studio")
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action = 'store_true', help = 'Enable verbose output')
+    args = parser.parse_args()
+    
+    
+    client = genai.Client(api_key = api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=messages)
+    
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
+
+
